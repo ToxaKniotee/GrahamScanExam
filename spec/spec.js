363f820e52561974b1fd8ba82a7d776f5e2e57c2
Toxa Kniotee <toxa_kniotee@outlook.com>
2015-09-10 10:49:51 -0500

    Find pivot function

--- spec/spec.js
+++ spec/spec.js
@@ -14,3 +14,32 @@ describe('nonTurnRight', function() {
         expect(ccw({x: 0, y: 0}, {x: 0, y: 1}, {x: 0, y: 2})).toEqual(true);
     });
 });
+
+describe('Find pivot index', function() {
+   it('Should find the only minimum y value', function() {
+        var points = [
+            {x: 21, y: 61}, /* 0 */
+            {x: 90, y: 89}, /* 1 */
+            {x: 84, y: 48}, /* 2 */
+            {x: 49, y: 30}, /* 3 */
+            {x: 14, y: 25}, /* 4 */
+            {x:  3, y: 21}, /* 5 */
+            {x: 95, y: 75}  /* 6 */
+        ];
+       expect(findPivot(points)).toEqual(5);
+   });
+
+   it('Should return the x min value in case of duplicate y min value', function() {
+        var points = [
+            {x: 21, y: 61}, /* 0 */
+            {x: 90, y: 89}, /* 1 */
+            {x: 84, y: 48}, /* 2 */
+            {x: 49, y: 30}, /* 3 */
+            {x: 14, y: 25}, /* 4 */
+            {x:  3, y: 21}, /* 5 */
+            {x:  2, y: 21}, /* 6 */
+            {x: 95, y: 75}  /* 7 */
+        ];
+       expect(findPivot(points)).toEqual(6);
+   });
+});
--- src/graham_scan.js
+++ src/graham_scan.js
@@ -9,3 +9,36 @@ function ccw(p1, p2, p3) {
     var difference = (p2.x - p1.x) * (p3.y - p1.y) - (p2.y - p1.y) * (p3.x - p1.x);
     return (difference >= 0) ? true : false;
 }
+
+function findPivot(points) {
+    /* y variables */
+    var y_min = points[0].y,
+        y_index = 0,
+        y_count = 0;
+
+    /* x variables */
+    var x_min = points[0].x,
+        x_index = 0,
+        x_count = 0;
+
+    for (var i = 0; i < points.length; i++) {
+        if (points[i].y == y_min) {
+            y_count++;
+        } else if (points[i].y < y_min) {
+            y_min = points[i].y;
+            y_count = 0;
+            y_index = i;
+        }
+
+        if (points[i].x == x_min) {
+            x_count++;
+        } else if (points[i].x < x_min) {
+            x_min = points[i].x;
+            x_count = 0;
+            x_index = i;
+        }
+    }
+
+    if (y_count == 0) return y_index;
+    return x_index;
+}
